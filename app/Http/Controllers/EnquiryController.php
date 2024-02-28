7309828762f366894176af68bf09fa1f5206e34f
Robin <Robin@Shreerajs-MacBook-Air.local>
2024-02-28 07:21:24 +0545

    Database and seeder setup

---
+++ app/Http/Controllers/EnquiryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Enquiry;
+use Illuminate\Http\Request;
+
+class EnquiryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Enquiry $enquiry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Enquiry $enquiry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Enquiry $enquiry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Enquiry $enquiry)
+    {
+        //
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'category_id'];
+
+    public function catagory()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function enquiries()
+    {
+        return $this->hasMany(Enquiry::class);
+    }
+    public function productGalaries()
+    {
+        return $this->hasMany(ProductGallery::class);
+    }
+}
