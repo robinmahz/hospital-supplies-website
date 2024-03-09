c5522257eb980c22ead0d2b299eacb0144bf510b
robinmahz <Robin@Shreerajs-MacBook-Air.local>
2024-03-10 01:11:05 +0545

    landing page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
@@ -11,7 +12,8 @@ class LandingPageController extends Controller
      */
     public function index()
     {
-        return view("welcome");
+        $products = Product::paginate(9);
+        return view("welcome", compact('products'));
     }
 
     /**
--- resources/views/hospital/components/card.blade.php
+++ resources/views/hospital/components/card.blade.php
@@ -1,2 +1,8 @@
-<div><img src="{{ asset('images/logo.png') }}" alt=""></div>
-<h3>Name</h3>
+<div class=" grid justify-center rounded-xl">
+    <div class="rounded-xl overflow-hidden border-2 "><img src="{{ asset('images/logo.png') }}" alt="">
+    </div>
+    <div class="text-center my-8 px-8">
+        <h3 class=" font-bold text-2xl">{{ $product->name }}</h3>
+        <div class="mt-2 text-gray-600">{{ $product->description }}</div>
+    </div>
+</div>
